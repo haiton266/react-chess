714f8ceb4791a7c93650a181410d5d4b68056af9
Joker3052 <phuonghoangthanh3052@gmail.com>
2023-10-31 00:24:16 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-
+import ReactDOM from 'react-dom/client';
+// import reportWebVitals from './reportWebVitals';
 import './index.css';
 import Game from './components/game.js'
 
-
-ReactDOM.render(
-  <Game />,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Game/>
+  </React.StrictMode>
 );
-
+// reportWebVitals();
